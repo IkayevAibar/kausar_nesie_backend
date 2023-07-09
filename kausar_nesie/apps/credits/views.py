b90a44c6a4784b14a45e2545791429e5e721f17e
Aibar <aibarchik2019@gmail.com>
2023-07-09 13:24:46 +0600

    exception handler added

--- kausar_nesie/apps/credits/views.py
+++ kausar_nesie/apps/credits/views.py
@@ -72,13 +72,13 @@ class CreditViewSet(viewsets.ModelViewSet):
         monthly_commission_in = 0
         data = self.calculate_payment(percent_rate, loan_term, loan_amount, commission_rate, days_in_first_payment, monthly_commission_in)
         
-        import os
-        return Response(os.getcwd())
         try:
             document = Document('/apps/credits/utils/template.docx')
         except:
-            document = Document('kausar_nesie/apps/credits/utils/template.docx')
-        
+            try:
+                document = Document('kausar_nesie/apps/credits/utils/template.docx')
+            except:
+                raise Response({"error_message":"Шаблон Документа не найден"},status=status.HTTP_404_NOT_FOUND)
 
         field_name_00 = "_director_full_name_"
         field_value_00 = "Директор Тестовик"
