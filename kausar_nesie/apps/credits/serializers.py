ec61e33cc02ac78e2a430c286e5480c45537fd58
Aibar <aibarchik2019@gmail.com>
2023-07-24 17:04:24 +0600

    credit serializer get updated

--- kausar_nesie/apps/credits/serializers.py
+++ kausar_nesie/apps/credits/serializers.py
@@ -9,7 +9,6 @@ class CreditSerializer(serializers.ModelSerializer):
     """Credit Create/Update"""
     request = serializers.IntegerField(read_only=True)
     date_close = serializers.DateField(read_only=True)
-    date_sign = serializers.DateField(read_only=True)
     class Meta:
         model = Credit
         fields = "__all__"
